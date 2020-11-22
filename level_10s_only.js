ce9de95268b184ffebb500fcd4f1acbfe10c927a
getfutureproof-admin <admin@getfutureproof.co.uk>
2020-11-22 23:37:38 +0000

    full oo level 10s app

--- level_10s_only.js
+++ level_10s_only.js
@@ -1,5 +1,6 @@
 // -------------------------------------------------
 // Run `node level_10s_only.js` to access the vault!
+// See lines 78+ for the entry point.
 // -------------------------------------------------
 // -------------------------------------------------
 
@@ -9,58 +10,71 @@ const ui = require('readline').createInterface({
     output: process.stdout
 });
 
-
 // Defining a custom Error by extending the built in Error object
 class SHIELDError extends Error {
     constructor(msg){
-        super(`AUTHORITIES HAVE BEEN ALERTED! ${msg}!`);
+        super(`\nAUTHORITIES HAVE BEEN ALERTED! ${msg}!\n`);
         this.name = 'SHIELDError';
     };
 
     shout(){ 
         console.clear()
-        console.log(`I am a ${this.name}!\n`.toUpperCase())
+        console.log(`I am a ${this.name}!`.toUpperCase())
     }
 };
 
 
-// Top Secret Vault App!
+// Level 10 Access List!
 const level10s = [{name: "Fury", faveColour: "red"}, {name: "Pierce", faveColour: "blue"}];
 
-console.clear()
-ui.question('\nWelcome to the Top Secret Info Vault. What is your last name?\n', input => {
-    try {
-        let user = level10s.find(u => u.name.toLowerCase() === input.toLowerCase());
-        if(user){
-            nextQuestion(user);
-        } else {
-            throw new Error(`Hey, you can't be here!`);
-        } ;
-    } catch (err) {
-        console.log(err.message); // Experiment with err, err.name, err.message, err.stack
+// Top Secret Vault App!
+class SecretVaultAccess {
+
+    async run(){
+        console.clear()
+        this.firstQuestion();
+    }
+
+    firstQuestion(){
+        ui.question('\nWelcome to the Top Secret Info Vault. What is your last name?\n', input => {
+            try {
+                this.user = level10s.find(u => u.name.toLowerCase() === input.toLowerCase());
+                if(!this.user){throw new Error(`Hey, you can't be here, ${input[0].toUpperCase() + input.slice(1)}!`);}
+                this.nextQuestion()
+            } catch (err) {
+                this.errorAndClose(err)
+            };
+        })
+    }
+
+    nextQuestion(){
+        console.log(`\nWelcome back, ${this.user.name}`);
+        ui.question('What is your favourite colour?\n', input => {
+            try {
+                if(input === this.user.faveColour){
+                    console.log('Excellent, you may proceed...');
+                    this.showTopSecretInfo();
+                } else {
+                    throw new SHIELDError(`${this.user.name}'s favourite colour would never be ${input}! Get outta here!`);
+                } 
+            } catch (err) {
+                err.shout() // custom error method
+                this.errorAndClose(err)
+            };
+        })
+    };
+
+    errorAndClose(err){
+        console.log('\n', err.message, '\n'); // Experiment with err, err.name, err.message, err.stack
+        ui.close();
+    }
+    
+    showTopSecretInfo(){
+        console.log("\nVisit https://marvelcinematicuniverse.fandom.com/wiki/Marvel_Cinematic_Universe_Wiki for the Top Secret Info\n");
         ui.close();
     };
-})
+}
 
-function nextQuestion(user){
-    console.log(`\nWelcome back, ${user.name}`);
-    ui.question('What is your favourite colour?\n', input => {
-        try {
-            if(input === user.faveColour){
-                console.log('Excellent, you may proceed...');
-                showTopSecretInfo();
-            } else {
-                throw new SHIELDError(`${user.name}'s favourite colour would never be ${input}! Get outta here!\n`);
-            } 
-        } catch (err) {
-            err.shout() // custom error method
-            console.log(err.message); // Experiment with err, err.name, err.message, err.stack
-            ui.close();
-        };
-    })
-};
-
-function showTopSecretInfo(){
-    console.log("\nVisit https://marvelcinematicuniverse.fandom.com/wiki/Marvel_Cinematic_Universe_Wiki for the Top Secret Info\n");
-    ui.close();
-};
+// Start a new instance of SecretVaultAccess and run it
+const app = new SecretVaultAccess()
+app.run()
