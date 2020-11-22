7654ddccc0d995e1c0042d9a9bd1f54748610ee5
getfutureproof-admin <admin@getfutureproof.co.uk>
2020-11-22 20:42:35 +0000

    tidy level 10s

--- level_10s_only.js
+++ level_10s_only.js
@@ -3,7 +3,7 @@
 // -------------------------------------------------
 // -------------------------------------------------
 
-// Using the built in readline module to get user input. prompt-sync can give a cleaner experience.
+// Using the built in readline module to get user input. prompt-sync or inquirer can give a cleaner experience.
 const ui = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
@@ -16,13 +16,19 @@ class SHIELDError extends Error {
         super(`AUTHORITIES HAVE BEEN ALERTED! ${msg}!`);
         this.name = 'SHIELDError';
     };
+
+    shout(){ 
+        console.clear()
+        console.log(`I am a ${this.name}!\n`.toUpperCase())
+    }
 };
 
 
 // Top Secret Vault App!
 const level10s = [{name: "Fury", faveColour: "red"}, {name: "Pierce", faveColour: "blue"}];
 
-ui.question('Welcome to the Top Secret Info Vault. What is your last name?\n', input => {
+console.clear()
+ui.question('\nWelcome to the Top Secret Info Vault. What is your last name?\n', input => {
     try {
         let user = level10s.find(u => u.name.toLowerCase() === input.toLowerCase());
         if(user){
@@ -37,16 +43,17 @@ ui.question('Welcome to the Top Secret Info Vault. What is your last name?\n', i
 })
 
 function nextQuestion(user){
-    console.log(`Welcome back, ${user.name}`);
+    console.log(`\nWelcome back, ${user.name}`);
     ui.question('What is your favourite colour?\n', input => {
         try {
             if(input === user.faveColour){
                 console.log('Excellent, you may proceed...');
                 showTopSecretInfo();
             } else {
-                throw new SHIELDError(`${user.name}'s favourite colour would never be ${input}! Get outta here!`);
+                throw new SHIELDError(`${user.name}'s favourite colour would never be ${input}! Get outta here!\n`);
             } 
         } catch (err) {
+            err.shout() // custom error method
             console.log(err.message); // Experiment with err, err.name, err.message, err.stack
             ui.close();
         };
@@ -54,6 +61,6 @@ function nextQuestion(user){
 };
 
 function showTopSecretInfo(){
-    console.log("Visit https://marvelcinematicuniverse.fandom.com/wiki/Marvel_Cinematic_Universe_Wiki for the Top Secret Info");
+    console.log("\nVisit https://marvelcinematicuniverse.fandom.com/wiki/Marvel_Cinematic_Universe_Wiki for the Top Secret Info\n");
     ui.close();
 };
